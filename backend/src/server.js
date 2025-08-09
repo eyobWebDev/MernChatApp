0db0e4ce3f6715cf68913e69e42eab311744dee2
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-08-09 11:17:08 +0300

    changed posiition of static frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,6 +20,12 @@ app.use(express.json({limit: '50mb'}))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
+app.use("/api/auth", authRouter)
+app.use("/api/messages", messageRouter)
+app.use("/api/groups", groupChatRouter)
+app.use("/api/groups/message", groupMessageRouter)
+
+
 const __dirname = path.resolve()
 if(process.env.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
@@ -28,11 +34,6 @@ if(process.env.NODE_ENV == "production"){
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
-
-app.use("/api/auth", authRouter)
-app.use("/api/messages", messageRouter)
-app.use("/api/groups", groupChatRouter)
-app.use("/api/groups/message", groupMessageRouter)
 
 
 server.listen(process.env.PORT, () => {
