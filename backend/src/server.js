e4a9e0c65702ac9290dec4d1a0226f622676c1e4
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-08-12 15:30:40 +0300

    added cookieparser

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,8 +16,8 @@ app.use(cors({
     origin: "https://mern-chat-app-rho-blue.vercel.app",
     credentials: true
 }))
-app.use(express.json({limit: '50mb'}))
 app.use(cookieParser())
+app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/auth", authRouter)
