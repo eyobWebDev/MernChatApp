1037a8c5764e8dad8c594de6eb68b26a85e32c0e
Eyob Tilahun <eyobdevmedia@gmail.com>
2025-08-19 14:11:25 +0300

    make cookie secure to true

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 3600 * 1000,
         httpOnly: true,
         sameSite: "none",
-        secure: process.env.NODE_END == "production"
+        secure: process.env.NODE_ENV == "production"
     })
     
     return token
